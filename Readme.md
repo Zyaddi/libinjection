a0936adad77a37e9bf3176c3091b5764d11e73ff
Zyaddi <135063805+Zyaddi@users.noreply.github.com>
2023-05-30 21:28:40 +0200

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## LIB INJECTION METHOD
+
+1- add your lib & inject files in /data/local/tmp path
+2- exucte "run.sh"
+3- done :)
---
+++ run.sh
@@ -0,0 +1,3 @@
+su -c am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
+sleep 4
+su -c /data/local/tmp/injector com.tencent.ig /data/local/tmp/libHack.so
